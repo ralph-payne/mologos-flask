885d94135ed6f5d1f83c6d678c685b526f0b5de4
ralph <ralphpayne@gmail.com>
2020-09-25 13:27:33 +0100

    Add Heroku config

--- app/__init__.py
+++ app/__init__.py
@@ -27,13 +27,16 @@ def create_app(config_name):
 
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
-    # print(f'Successfully created app with config name: {config_name}')
     
     # bootstrap.init_app(app)
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+
+    if app.config['SSL_REDIRECT']:
+        from flask_sslify import SSLify
+        sslify = SSLify(app)
 
     # Main blueprint registration
     from.main import main as main_blueprint
